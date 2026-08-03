17e09eb93b89d44f8c393e4e67dac93e7093ed01
nidhibidwal <nidhi9005bidwal@gmail.com>
2026-08-03 00:10:48 -0700

    Fix syntax errors in app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,23 @@
-from flask*import Flask
+from flask import Flask
 import requests
-impor* os
+import os
 
 app = Flask(__name__)
 
-@app.r*ute("/")
+@app.route("/")
 def home():
     try:
-    *   response = requests.get(
-      *     os.environ["BACKEND_URL"]
-   *    )
+        response = requests.get(
+            os.environ["BACKEND_URL"]
+        )
 
         return f"""
-       *<h1>Hello from Frontend</h1>
-     *  <h2>{response.text}</h2>
-       *"""
+        <h1>Hello from Frontend</h1>
+        <h2>{response.text}</h2>
+        """
 
     except Exception as e:
-  *     return f"Backend Connection F*iled: {str(e)}"
+        return f"Backend Connection Failed: {str(e)}"
 
-if __name__ == "_*main__":
-    app.run(host="0.0.0.0*, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
